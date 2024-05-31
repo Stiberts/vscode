fabf7eb71b745caec914e27996f120b8b7611cd6
Stian Berthelsen <stian.berthelsen@outlook.com>
2024-05-31 09:23:56 +0200

    updates

--- javascript_if_else/oppgave2.js
+++ javascript_if_else/oppgave2.js
@@ -1,1 +1,30 @@
-let myInput = document.getElementById("pOrd");
+function logIn() {
+    let bruker = document.getElementById("brukernavn").value;
+    let passord = document.getElementById("passord").value;
+
+    let registrertBruker = "Stian";
+    let registrertPassord = "Berthelsen";
+
+    if (bruker == registrertBruker && passord == registrertPassord) {
+        document.getElementById("output").innerHTML = `Velkommen! Du er logget inn som ${bruker}.`;
+        document.getElementById("output").classList.add("verified");
+        document.getElementById("output").classList.remove("unverified");
+        //document.getElementById("output").style.backgroundColor = "green";
+        //document.getElementById("output").style.color = "white";
+        //document.getElementById("output").style.padding = "4px";
+        //document.getElementById("output").style.marginTop = "5px";
+        //document.getElementById("output").style.borderRadius = "4px";
+        //document.getElementById("output").style.width = "300px";
+    }
+    else {
+        document.getElementById("output").innerHTML = `Brukernavn eller passord er feil.`;
+        document.getElementById("output").classList.remove("verified");
+        document.getElementById("output").classList.add("unverified");
+        //document.getElementById("output").style.backgroundColor = "red";
+        //document.getElementById("output").style.color = "white";
+        //document.getElementById("output").style.padding = "4px";
+        //document.getElementById("output").style.marginTop = "5px";
+        //document.getElementById("output").style.borderRadius = "4px";
+        //document.getElementById("output").style.width = "300px";
+    }
+}
--- javascript_if_else/oppgave3.js
+++ javascript_if_else/oppgave3.js
@@ -1,7 +1,22 @@
 function lagEpost() {
     let fornavn = document.getElementById("fnavn").value;
-    let mellomnavn = document.getElementById("mnavn").value;
+    let partFornavn = fornavn.slice(0,3);
     let etternavn = document.getElementById("enavn").value;
+    let mellomnavn = document.getElementById("mnavn").value;
+    let partMellomnavn = mellomnavn.slice(0,2);
 
-    document.getElementById("melding").innerHTML = `Du skrev inn ${fornavn} + ${mellomnavn} + ${etternavn}`;
+    let dinEpost = partFornavn + "." + partMellomnavn + "." + etternavn + "@" + "alf.no" ; 
+
+    let dinEpost2 = partFornavn + "-" + etternavn + "@" + "alf.no";
+
+    if (mellomnavn == "") {
+        document.getElementById("melding").innerHTML = `Din e-post er: ${dinEpost2}`;
+        document.getElementById("melding").classList.add("greenbox");
+
+    }
+
+    else {
+        document.getElementById("melding").innerHTML = `Din e-post er ${dinEpost}`;
+        document.getElementById("melding").classList.add("greenbox");
+    }
 }
