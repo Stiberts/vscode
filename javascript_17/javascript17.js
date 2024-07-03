5a8325836c21880e2ef14db48cf5bb0f40a14694
Stian Berthelsen <stian.berthelsen@outlook.com>
2024-07-03 13:23:15 +0200

    Javascript 17, oppgaver 1 - 3, hent API, skriv ut info med Async & Await, Array med strømpriser som sorteres med reduce og filter metodene. Skrives ut til nettside

--- javascript_17/javascript17.js
+++ javascript_17/javascript17.js
@@ -16,7 +16,7 @@ function landInfo() {
                     <hr>
             `)
         })
-
+        .finally(() => utskrift.innerHTML += `Takk for idag, ha en trivelig dag.`);
 }
 
 // Oppgave 1 B. Samme funksjon men med Async og Await //
@@ -43,4 +43,83 @@ async function landInfo2() {
         
         console.error('Feil ved henting av data:', error);
     }
+
+    finally {
+        utskrift.innerHTML += `Takk for idag, ha en fisefin dag.`;
+    }
+
+        
+}
+
+// Oppgave 2 a. Json-fil fra API //
+
+function hentAPI() {
+    let utskrift = document.getElementById("utskrift3");
+    let url = "https://jsonplaceholder.typicode.com/comments/";
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            
+            data.forEach((innhold) => utskrift.innerHTML += `
+                <ul>
+                    <li><h4>Navn: ${innhold.name}</li></h4>
+                    <li><h4>Epost: ${innhold.email}</li></h4>
+                    <li><h4>Tekst: ${innhold.body}</li></h4>
+                    <hr>
+            `)
+        })
+
+        .catch(() => utskrif.innerHTML = `Dang! Fikk ikke tak i API.`)
+        .finally(() => utskrift.innerHTML += `Takk for idag, ha en trivelig dag.`);
+}
+
+// Oppgave 2 B. Samme funksjon men med Async og Await //
+
+async function hentAPI2() {
+    let utskrift = document.getElementById("utskrift4");
+
+    try {
+        let res = await fetch("https://jsonplaceholder.typicode.com/comments/");
+        let data = await res.json();
+
+        data.forEach((innhold) => {
+            utskrift.innerHTML += `
+                <ul>
+                    <li><h4>Navn: ${innhold.name}</li></h4>
+                    <li><h4>Epost: ${innhold.email}</li></h4>
+                    <li><h4>Tekst: ${innhold.body}</li></h4>
+                    <hr>
+                </ul>
+            `;
+        });
+    } catch (error) {
+        
+        console.error('Feil ved henting av data fra API:', error);
+    }
+
+    finally {
+        utskrift.innerHTML += `Takk for idag, ha en fisefin dag.`;
+    }
+
+        
+}
+
+// Oppgave 3. Array med strømpriser //
+
+function stromPriser() {
+let strom = [1.3 , 2.3, 9.0, 3.3, 2.4, 5.5, 6.7, 9.9, 2.3, 3.4, 4.5, 5.6, 7.7, 6.3 , 7.3, 3.0, 6.3, 5.4, 3.5, 6.9, 10.2, 2.3, 3.4, 4.5, 4.6, 2.7, 3.4, 5.6, 8.9, 9.4, 2.2, 3.4];
+let utskrift = document.getElementById("utskrift5");
+
+let total = strom.reduce((a,b) => a + b, 0);
+let gjennomsnitt = total / strom.length;
+let priserOverTo = strom.filter(item => item > 2).toSorted((a,b) => a - b);
+
+utskrift.innerHTML = `
+Total strømpris: ${Math.round(total)} kr,- <br> 
+Gjennomsnittspris: ${Math.round(gjennomsnitt)} kr,- <br> 
+Priser over kr.2,- (stigende order): ${priserOverTo.join(', ')}
+`;
+
+
 }
