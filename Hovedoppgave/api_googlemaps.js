494ae66e13883b66e81d7f9fdd6c70ff3ef3e492
Stian Berthelsen <stian.berthelsen@outlook.com>
2024-07-12 13:38:20 +0200

    Hovedoppgave, Google Maps fikset ved å generere ny API-nøkkel!

--- Hovedoppgave/api_googlemaps.js
+++ Hovedoppgave/api_googlemaps.js
@@ -1,29 +1,27 @@
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyA3O8LXVHuHwiQJ6jAYEHjY9SFEoBU4Orw",
-    v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
-  });
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyCxXyrT1OVlXz2BW3KDtc6Im-vU1izE0xE", v: "weekly"});
 
-  // Initialize and add the map
+// Initialize and add the map
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  // The location of Bergen
+  const position = { lat: 60.397076, lng: 5.324383 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  console.log("Kart", Map);
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  console.log("marker", AdvancedMarkerElement);
 
-  // The map, centered at Uluru
+  // The map, centered at Bergen
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 10,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
+  // The marker, positioned at Bergen
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
@@ -32,24 +30,3 @@ async function initMap() {
 }
 
 initMap();
-
-// The location of Uluru
-const position = { lat: -25.344, lng: 131.031 };
-// Request needed libraries.
-//@ts-ignore
-const { Map } = await google.maps.importLibrary("maps");
-const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-// The map, centered at Uluru
-map = new Map(document.getElementById("map"), {
-  zoom: 4,
-  center: position,
-  mapId: "DEMO_MAP_ID",
-});
-
-// The marker, positioned at Uluru
-const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
